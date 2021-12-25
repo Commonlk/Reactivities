d32b5181cd7874b19044c30f5ff062402a94ad17
Victor Azevedo <vitinsilva0xd@gmail.com>
2021-12-25 02:54:17 -0300

    Add functions to handle CRUD operations and refactor

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Header, List } from "semantic-ui-react";
+import { v4 as uuid } from "uuid";
+import { Container } from "semantic-ui-react";
+// Interface
 import { Activity } from "../models/activty";
+// Components
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<
+    Activity | undefined
+  >(undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     axios
@@ -14,16 +23,55 @@ function App() {
       });
   }, []);
 
-  return (
-    <div>
-      <Header as="h2" icon="users" content="Reactivities" />
+  const handleSelectedActivity = (id: string): void => {
+    setSelectedActivity(activities.find((activity) => activity.id === id));
+  };
 
-      <List>
-        {activities.map((activity) => {
-          return <List.Item key={activity.id}>{activity.title}</List.Item>;
-        })}
-      </List>
-    </div>
+  const handleCancelSelectedActivity = (): void => {
+    setSelectedActivity(undefined);
+  };
+
+  const handleFormOpen = (id?: string): void => {
+    id ? handleSelectedActivity(id) : handleCancelSelectedActivity();
+    setEditMode(true);
+  };
+
+  const handleFormClose = (): void => {
+    setEditMode(false);
+  };
+
+  const handleCreateOrEditActivity = (activity: Activity): void => {
+    activity.id
+      ? setActivities([
+          ...activities.filter((item) => item.id !== activity.id),
+          activity,
+        ])
+      : setActivities([...activities, { ...activity, id: uuid() }]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+  };
+
+  const handleDeleteActivity = (id: string): void => {
+    setActivities([...activities.filter((item) => item.id !== id)]);
+  };
+
+  return (
+    <>
+      <NavBar openForm={handleFormOpen} />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectedActivity}
+          cancelSelectActivity={handleCancelSelectedActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
+    </>
   );
 }
 
