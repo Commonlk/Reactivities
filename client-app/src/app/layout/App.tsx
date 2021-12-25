0934a8930a6ba871bc6b1347e166b2104f110a1a
Victor Azevedo <vitinsilva0xd@gmail.com>
2021-12-25 00:41:37 -0300

    Add files structure and reorganize

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import axios from "axios";
 import { Header, List } from "semantic-ui-react";
+import { Activity } from "../models/activty";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/activities").then((response) => {
-      console.log(response);
-      setActivities(response.data);
-    });
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
   }, []);
 
   return (
@@ -18,7 +19,7 @@ function App() {
       <Header as="h2" icon="users" content="Reactivities" />
 
       <List>
-        {activities.map((activity: any) => {
+        {activities.map((activity) => {
           return <List.Item key={activity.id}>{activity.title}</List.Item>;
         })}
       </List>
