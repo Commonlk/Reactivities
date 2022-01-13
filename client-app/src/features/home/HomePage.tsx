e9e714f2b87bf908cc53b21691cf4a9f557f7fd5
Victor <vitinsilva0xd@gmail.com>
2022-01-13 13:13:48 -0300

    Add link to go the activities dashboard

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 
 const HomePage = () => {
@@ -6,6 +7,9 @@ const HomePage = () => {
     <div>
       <Container style={{ marginTop: "7em" }}>
         <h1>Home page</h1>
+        <h3>
+          Go to <Link to="/activities">Activities</Link>
+        </h3>
       </Container>
     </div>
   );
