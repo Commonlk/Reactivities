c83622561868e52570dca9462f543857e6a395ad
Victor <vitinsilva0xd@gmail.com>
2022-01-14 10:49:57 -0300

    Import and implements test errors and toast

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -9,12 +9,15 @@ import { Route, useLocation } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import TestErrors from "../../features/errors/TestErrors";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const location = useLocation();
 
   return (
     <>
+      <ToastContainer position="bottom-right" hideProgressBar />
       <Route exact path="/" component={HomePage} />
       <Route
         path={"/(.+)"}
@@ -29,6 +32,7 @@ function App() {
                 path={["/createActivity", "/manage/:id"]}
                 component={ActivityForm}
               />
+              <Route path="/errors" component={TestErrors} />
             </Container>
           </>
         )}
