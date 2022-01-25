a1a3315667fba88693bddfda14c7d2c8d1f47caf
Victor Azevedo <vitinsilva0xd@gmail.com>
2022-01-25 15:35:36 -0300

    Add identity service to configuration

--- API/Startup.cs
+++ API/Startup.cs
@@ -11,7 +11,6 @@ namespace API
         public Startup(IConfiguration config)
         {
             _config = config;
-
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -22,6 +21,7 @@ namespace API
                 config.RegisterValidatorsFromAssemblyContaining<Create>();
             });
             services.AddApplicationServices(_config);
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
