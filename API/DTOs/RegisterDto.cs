9be65472b64720cc54c9af71f374bd327e22b8b1
Victor Azevedo <vitinsilva0xd@gmail.com>
2022-01-25 18:03:31 -0300

    Add controller and dtos to account handling

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,11 @@
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Passwrod { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
