7150601819a205f0d70aeab0151610cdcb219ca3
Victor Azevedo <vitinsilva0xd@gmail.com>
2022-01-30 17:56:58 -0300

    Update to use the activityformvalues class

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -12,29 +12,18 @@ import MyTextArea from "../../../app/common/form/MyTextArea";
 import MySelectInput from "../../../app/common/form/MySelectInput";
 import { categoryOptions } from "../../../app/common/options/categoryOptions";
 import MyDateInput from "../../../app/common/form/MyDateInput";
-import { Activity } from "../../../app/models/activty";
+import { ActivityFormValues } from "../../../app/models/activty";
 
 const ActivityForm = observer(() => {
   const history = useHistory();
   const { activityStore } = useStore();
-  const {
-    loadActivity,
-    createActivity,
-    updateActivity,
-    loading,
-    loadingInitial,
-  } = activityStore;
+  const { loadActivity, createActivity, updateActivity, loadingInitial } =
+    activityStore;
   const { id } = useParams<{ id: string }>();
 
-  const [activity, setActivity] = useState<Activity>({
-    id: "",
-    title: "",
-    category: "",
-    description: "",
-    date: null,
-    city: "",
-    venue: "",
-  });
+  const [activity, setActivity] = useState<ActivityFormValues>(
+    new ActivityFormValues()
+  );
 
   const validationSchema = Yup.object({
     title: Yup.string().required("The activity title is required"),
@@ -47,11 +36,14 @@ const ActivityForm = observer(() => {
 
   useEffect(() => {
     // activity! => *! ignores typescript error from maybe undefined
-    if (id) loadActivity(id).then((activity) => setActivity(activity!));
+    if (id)
+      loadActivity(id).then((activity) =>
+        setActivity(new ActivityFormValues(activity))
+      );
   }, [id, loadActivity]);
 
-  const handleFormSubmit = (activity: Activity) => {
-    if (activity.id.length === 0) {
+  const handleFormSubmit = (activity: ActivityFormValues) => {
+    if (!activity.id) {
       let newActivity = {
         ...activity,
         id: uuid(),
@@ -98,7 +90,7 @@ const ActivityForm = observer(() => {
             <MyTextInput placeholder="Venue" name="venue" />
             <Button
               disabled={isSubmitting || !dirty || !isValid}
-              loading={loading}
+              loading={isSubmitting}
               floated="right"
               positive
               type="submit"
