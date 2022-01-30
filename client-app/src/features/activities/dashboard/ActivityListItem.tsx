7fcc225ed344ea86c55f4837da69ea481fc83b70
Victor Azevedo <vitinsilva0xd@gmail.com>
2022-01-30 18:20:29 -0300

    Fix user image not being fully round

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -23,7 +23,12 @@ const ActivityListItem = ({ activity }: Props) => {
         )}
         <Item.Group>
           <Item>
-            <Item.Image size="tiny" circular src="/assets/images/user.png" />
+            <Item.Image
+              style={{ marginBottom: 5 }}
+              size="tiny"
+              circular
+              src="/assets/images/user.png"
+            />
             <Item.Content>
               <Item.Header as={Link} to={`/activities/${activity.id}`}>
                 {activity.title}
