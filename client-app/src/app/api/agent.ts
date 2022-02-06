4d8280b0543711f754c74233bffb9371432fd359
Victor Azevedo <vitinsilva0xd@gmail.com>
2022-02-06 12:37:01 -0300

    Add method to update following and list

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -100,6 +100,10 @@ const Profiles = {
   updateProfile: (profile: Partial<Profile>) =>
     requests.put<void>("/profiles", profile),
   deletePhoto: (id: string) => requests.del<void>(`/photos/${id}`),
+  updateFollowing: (username: string) =>
+    requests.post(`/follow/${username}`, {}),
+  listFollowings: (username: string, predicate: string) =>
+    requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`),
 };
 
 const agent = {
